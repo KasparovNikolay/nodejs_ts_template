801dc0997626bdc38f0de8fc62a2ee017cb2f989
Kasaprov <Kasparov19ya@yandex.ru>
2022-11-07 21:35:53 +0300

    apply style lint

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { config } from 'dotenv';
-import app from "./app";
-import {Request, Response} from "express";
+import app from './app';
+import { Request, Response } from 'express';
 
 config();
 const PORT = process.env.PORT || 3001;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World")
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running in http://localhost:${PORT}`)
+  console.log(`Server is running in http://localhost:${PORT}`);
 });
