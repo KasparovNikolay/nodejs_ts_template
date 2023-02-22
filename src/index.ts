2bc6c5f0f16513e9c060238a9e715fb065e81686
Kasaprov <Kasparov19ya@yandex.ru>
2023-02-22 23:37:29 +0300

    add listener for user auth

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 import { config } from 'dotenv';
+import crypto from 'crypto';
 import app from './app';
 import { Request, Response } from 'express';
 
@@ -7,9 +8,35 @@ config();
 
 const PORT = process.env.PORT || 3001;
 
+const db: Array<{ login: string; password: string }> = [
+  {
+    login: 'qwerty',
+    password: '123456',
+  },
+  {
+    login: 'kolya',
+    password: 'qwerty',
+  },
+];
+
 app.post('/', (req: Request, res: Response) => {
-  console.log('req', req.body);
-  res.send('Hello World');
+  try {
+    const { login, password } = req.body;
+
+    const index = db.findIndex(
+      (item) => item.login === login && item.password === password
+    );
+
+    if (index !== -1) {
+      res.send({ success: true, user_id: crypto?.randomUUID() });
+      return;
+    }
+
+    res.send({ success: false });
+  } catch (e) {
+    console.log(e);
+    res.send({ success: false });
+  }
 });
 
 app.listen(PORT, () => {
